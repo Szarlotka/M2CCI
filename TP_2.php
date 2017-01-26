c1d2b6b6ec0d478b195a7c8698bc7be38125bbe2
Szarlotka <leon.charlott@gmail.com>
2017-01-26 09:44:18 +0100

    Update TP_2.php

--- TP_2.php
+++ TP_2.php
@@ -1,29 +1,68 @@
 <?php
 session_start();
-if(!empty($_POST['sexe'])){ 
-foreach($_POST['sexe'] as $val) {
-		$_SESSION['sexe'] = $val;	
-};}
-echo $_session['sexe'];
+
+$_SESSION['sexe'] = $_POST['sexe'];
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['dateNaiss'] = $_POST['dateNaiss'];
+
+$_SESSION['nom'] = $_POST['nom'];
+$_SESSION['prenom'] = $_POST['prenom'];
+$_SESSION['telfixe'] = $_POST['telfixe'];
+$_SESSION['telmobile'] = $_POST['telmobile'];
+
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <form method="post"action="TP_3.php">
-<p>Mot de passe pour protéger vos données <font color="red">*</font><br>
-<input type="text" name="mdp"></p>
-<p>Adresse <font color="red">*</font><br>
-<input type="text" name="adresse"></p>
-<p>Complément d'adresse</p>
-<p>Bâtiment Escalier Etage Porte<br>
-<input type="text" name="bat"><input type="text" name="escalier"><input type="text" name="etage"><input type="text" name="porte"></p>
-<p>Code Postal <font color="red">* </font>Ville <font color="red">*</font><br>
-<input type="text" name="CP"><input type="text" name="ville"></p>
-<p>Pays<font color="red">*</font><br>
-<input type="text" name="pays"></p>
-<p><input type="submit" value="Retour"/>
-<input type="submit" value="Valider"/></p>
+<table>
+<tr>
+	<td colspan="4">Mot de passe pour protéger vos données <font color="red">*</font></td>
+</tr>
+<tr>
+	<td colspan="4"><input size="49" type="text" name="mdp"></td>
+</tr>
+<tr>
+	<td colspan="4">Adresse <font color="red">*</font></td>
+</tr>
+<tr>
+	<td colspan="4"><input size="49" type="text" name="adresse"></td>
+</tr>
+<tr>
+	<td colspan="4">Complément d'adresse</td>
+</tr>
+<tr>
+	<td>Bâtiment</td>
+	<td>Escalier</td>
+	<td>Etage</td>
+	<td>Porte</td>
+</tr>
+<tr>
+	<td><input size="8" type="text" name="bat"></td>
+	<td><input size="8" type="text" name="escalier"></td>
+	<td><input size="8" type="text" name="etage"></td>
+	<td><input size="8" type="text" name="porte"></td>
+</tr>
+</table>
+<table>
+<tr>
+	<td>Code Postal <font color="red">* </font></td>
+	<td>Ville <font color="red">*</font></t32
+</tr>
+<tr>
+	<td><input size="12" type="text" name="CP"></td>
+	<td><input size="32" type="text" name="ville"></td>
+</tr>
+<tr>
+	<td colspan="2">Pays<font color="red">*</font></td>
+</tr>
+<tr>
+	<td colspan="2"><input size="49" type="text" name="pays"></td>
+</tr>
+<tr>
+	<td><input type="submit" value="Retour"/></td>
+	<td><input type="submit" value="Valider"/></td>
+</tr>
+</table>
 </form>
-
 </html>
